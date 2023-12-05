b791054378a8009aac2967147f4e0b74b799ac82
vietpham-stvg <82923647+phamquangviet891@users.noreply.github.com>
2023-12-05 16:37:47 +0700

    update
    config cors

--- cs_se347/cs_se347/Program.cs
+++ cs_se347/cs_se347/Program.cs
@@ -5,10 +5,19 @@ namespace cs_se347
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(builder =>
+                {
+                    builder.WithOrigins("http://localhost:3000")
+                        .AllowAnyHeader()
+                        .AllowAnyMethod()
+                        .AllowCredentials();
+                });
+            });
+                    // Add services to the container.
 
-            // Add services to the container.
-
-            builder.Services.AddControllers();
+                    builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
@@ -16,16 +25,15 @@ namespace cs_se347
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
                 app.UseSwagger();
                 app.UseSwaggerUI();
-            }
+            app.MapGet("/", () => string.Format("Server Se347 - {0}", DateTime.Now));
 
             app.UseAuthorization();
 
 
             app.MapControllers();
+            app.UseCors();
 
             app.Run();
         }
